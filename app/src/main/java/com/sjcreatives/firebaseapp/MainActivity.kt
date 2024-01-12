a22907bc14a3ec9a79919bf9d9368f3f34e9741d
samueljuma <samueljuma1024@gmail.com>
2024-01-12 09:08:12 +0300

    Read From DB

--- app/src/main/java/com/sjcreatives/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/sjcreatives/firebaseapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sjcreatives.firebaseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.google.firebase.Firebase
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -17,11 +18,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val displayText: TextView = findViewById(R.id.txtView)
+
         //Firebase RealTime database reference
         database = Firebase.database.reference
 
         //write to database
-        database.child("Hey You").child("1").setValue("Hey")
+        database.child("bio").setValue("Hi, Samuel Juma here. Good to see you")
+
+        //Read data from db
+        val postListener = object : ValueEventListener{
+            override fun onDataChange(snapshot: com.google.firebase.database.DataSnapshot) {
+                val myData = snapshot.value
+                displayText.text = myData.toString()
+
+            }
+            override fun onCancelled(error: com.google.firebase.database.DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+        database.child("bio").addValueEventListener(postListener)
+
 
     }
 }
